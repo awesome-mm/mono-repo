f58d72deb50b433990e282c7e1f4ccd04e35ed0a
chan <opokonse123@naver.com>
2025-10-21 15:38:52 +0900

    공통 유틸 생성

---
+++ packages/utils/src/format/dateFormat.ts
@@ -0,0 +1,7 @@
+export const dateFormat = (date: Date, locale = 'ko-KR') => {
+  return new Intl.DateTimeFormat(locale, {
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+  }).format(date);
+};
